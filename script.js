f5d372f5692cc13a671cf4a99842bf56716d6759
Chris Burns <ckbzxz@gmail.com>
2020-10-30 17:46:47 -0400

    added functionality to clone and append time slots, and update time text:

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
+$(document).ready(function () {
+
+var timesOfDay = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var inputCounter = 0;
+
+$(".row").clone().appendTo(".container");
+$(".row").clone().appendTo(".container");
+$(".row").clone().appendTo(".container");
+//everytime the above line is called, it duplicates what is there, so now there are 8 rows
 
 
-// $(document).ready(function () {
+//the code below updates the time box to descend to from 9 - 5
+var timeContainer = document.querySelector(".container");
+var timeSlotList = timeContainer.getElementsByTagName("aside");
 
-// var timesOfDay = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-// var inputCounter = 0;
+for (let i = 0; i < timeSlotList.length; i++) {
+    if (i <= 2) {
+        timeSlotList[i].textContent = timesOfDay[i] + " AM";
+    } else {
+        timeSlotList[i].textContent = timesOfDay[i] + " PM";
+    }
+}
 
-// //generates timeslots from top of the page going down
-// for (let i = 0; i < timesOfDay.length; i++) {
-//     var timeSlot = $("<button>");
-//     timeSlot.addClass("time time-color col-lg-12");
-//     timeSlot.attr("data-time", timesOfDay[i]);
-//     timeSlot.text(timesOfDay[i]);
-//     timeSlot.attr("style", "height: 100px; text-align: left;");
-//     $(".container").append(timeSlot);
-//     timeSlot.append("<p class='innertext'>");
-//     var timeText = $(".innertext");
-//     timeText.attr("style", "text-align: center;");
-//     // timeSlot.append("<input class='timeInput'>");
-// }
-
-// $(".time").on("click", function () {
-//     var timeInput = $("<input>")
-//     inputCounter++;
-//     console.log(inputCounter)
-//     if (inputCounter <= 1) {
-//         $(this).append(timeInput);
-//     }
-//     console.log(timeInput.value);
-//     //selects the p tag inside the button and makes its text "HELLO"
-
-//     })
-
-// })
+})
 
