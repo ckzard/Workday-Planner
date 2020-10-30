0f5f58e3cbc622cf79743d4ea4fa582fd8fbb6f3
Chris Burns <ckbzxz@gmail.com>
2020-10-30 16:50:39 -0400

    created base outline for timeslots

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-$(document).ready(function () {
 
-var timesOfDay = [9, 10, 11, 12, 1, 2, 3, 4, 5];
 
-for (let i = 0; i < timesOfDay.length; i++) {
-    var timeSlot = $("<button>");
-    timeSlot.addClass("time time-color col-lg-12");
-    timeSlot.attr("data-time", timesOfDay[i]);
-    timeSlot.text(timesOfDay[i]);
-    timeSlot.attr("style", "height: 100px; text-align: left;");
-    $(".container").append(timeSlot);
-    timeSlot.append("<p class='innertext'>");
-    var timeText = $(".innertext");
-    timeText.attr("style", "text-align: center;");
-    // timeSlot.append("<input class='timeInput'>");
-}
-//generates timeslots from top of the page going down
-$(".time").on("click", function () {
-    $(this).children(".innertext").text("THING TO DO");
-    //selects the p tag inside the button and makes its text "HELLO"
+// $(document).ready(function () {
 
-    })
+// var timesOfDay = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+// var inputCounter = 0;
 
-})
+// //generates timeslots from top of the page going down
+// for (let i = 0; i < timesOfDay.length; i++) {
+//     var timeSlot = $("<button>");
+//     timeSlot.addClass("time time-color col-lg-12");
+//     timeSlot.attr("data-time", timesOfDay[i]);
+//     timeSlot.text(timesOfDay[i]);
+//     timeSlot.attr("style", "height: 100px; text-align: left;");
+//     $(".container").append(timeSlot);
+//     timeSlot.append("<p class='innertext'>");
+//     var timeText = $(".innertext");
+//     timeText.attr("style", "text-align: center;");
+//     // timeSlot.append("<input class='timeInput'>");
+// }
+
+// $(".time").on("click", function () {
+//     var timeInput = $("<input>")
+//     inputCounter++;
+//     console.log(inputCounter)
+//     if (inputCounter <= 1) {
+//         $(this).append(timeInput);
+//     }
+//     console.log(timeInput.value);
+//     //selects the p tag inside the button and makes its text "HELLO"
+
+//     })
+
+// })
 
